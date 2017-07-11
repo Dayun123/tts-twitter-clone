8163c8a55478429f1f84a3a2daca0e9263b42f29
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2017-07-11 10:41:52 -0500

    Add create and destroy actions to LikesController.
    Add nested route for tweets/likes in router.

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   get 'profiles/:id' => 'profiles#show', as: :profile
 
   resources :tweets
-  resources :relationships
-
+  resources :relationships, only: [:create, :destroy]
+  resources :tweets do
+    resource :likes, only: [:create, :destroy]
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
